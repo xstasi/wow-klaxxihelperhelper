cee924dee50269598385f00fb1f3a78bace2c10a
Alessandro Grassi <alessandro@aggro.it>
2014-01-03 20:40:59 +0100

    Only respond to whispers from the raid leader

--- KlaxxiHelperHelper.lua
+++ KlaxxiHelperHelper.lua
@@ -29,18 +29,20 @@ function KlaxxiHH.WhisperHandler(self, event, ...)
    -- If we receive a whisper
    if(event == 'CHAT_MSG_WHISPER') then
       local msg, sender = ...
-      -- If it contains "Your camp is", store the camp name
-      if string.find(msg, "Your camp is ") then
-         KlaxxiHH.Camp = string.sub(msg, strlen("Your camp is ") + 1)
-         KlaxxiHH.LiteralCamp = KlaxxiHH.MarkerTranslator(KlaxxiHH.Camp)
-         print("Got camp: " .. KlaxxiHH.Camp .. " (" .. KlaxxiHH.LiteralCamp .. "), sender: " .. sender )
-         KlaxxiHH.MoveWarning = mod:NewSpecialWarning('Move to ' .. KlaxxiHH.LiteralCamp .. '!')
-      elseif string.find(msg, "TO THE CAMP!") then
-         KlaxxiHH.MoveWarning:Show()
-      elseif string.find(msg, "GET OUT!") then
-         KlaxxiHH.OutWarning:Show()
-      else
-         print("not matched")
+      if(UnitIsGroupLeader(sender)) then
+        -- If it contains "Your camp is", store the camp name
+        if string.find(msg, "Your camp is ") then
+           KlaxxiHH.Camp = string.sub(msg, strlen("Your camp is ") + 1)
+           KlaxxiHH.LiteralCamp = KlaxxiHH.MarkerTranslator(KlaxxiHH.Camp)
+           print("Got camp: " .. KlaxxiHH.Camp .. " (" .. KlaxxiHH.LiteralCamp .. "), sender: " .. sender )
+           KlaxxiHH.MoveWarning = mod:NewSpecialWarning('Move to ' .. KlaxxiHH.LiteralCamp .. '!')
+        elseif string.find(msg, "TO THE CAMP!") then
+           KlaxxiHH.MoveWarning:Show()
+        elseif string.find(msg, "GET OUT!") then
+           KlaxxiHH.OutWarning:Show()
+        else
+           print("not matched")
+        end
       end
    end
 end    
